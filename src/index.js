7e138a5c35399b467a71f696a38cfd9eae594a58
ShiChenChong <sccong@outlook.com>
2017-07-03 14:57:01 +0800

    添加 side

--- src/index.js
+++ src/index.js
@@ -2,30 +2,43 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.less'
 import ParamsExample from './route/route.js';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import createHistory from 'history/createBrowserHistory'
 import configureStore from './store/configureStore.js';
-import { ConnectedRouter } from 'react-router-redux'
+import {ConnectedRouter} from 'react-router-redux'
+import Side from './component/Side/side.js'
+import {Layout} from 'antd';
+const {Header, Footer, Sider, Content} = Layout;
 
 const store = configureStore();
 const history = createHistory();
 
-class App extends Component{
-	constructor(props){
+class App extends Component {
+	constructor(props) {
 		super(props)
 	}
 	render() {
-		return(
-			  <Provider store={store} >
-				  	<ConnectedRouter history={history}>
-						{ ParamsExample() }
-					</ConnectedRouter>
-			  </Provider>
+		return (
+			<div>
+				<Layout style={{ height: '100vh' }}>
+					<Sider>
+						<Side />
+					</Sider>
+					<Layout>
+						<Provider store={store}>
+							<ConnectedRouter history={history}>
+								{ParamsExample()}
+							</ConnectedRouter>
+						</Provider>
+					</Layout>
+				</Layout>
+			</div>
 		)
 	}
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<App/>, document.getElementById('root'));
 
 if (module.hot) {
 	module.hot.accept();
